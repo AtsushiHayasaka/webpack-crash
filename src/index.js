14cc1a1c46d22f57cef4ae22181d8bad229ab089
AtsushiHayasaka <a.hayusaka0614@gmail.com>
2021-09-15 20:35:10 +0900

    apply uglifyjs-plugin

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import logo2 from './F66F1B12-EEE3-4FF6-A115-E7931D6DA55E.jpeg';
 import './style.css';
 import './style.scss';
 
+console.log('I am printing logs');
+
 ReactDOM.render(<div>Hello, React!</div>, document.getElementById('root'));
